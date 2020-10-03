c2db919f5e391e4fc5b39bc22fb5a4699b4d249d
Nikhil-dev13 <nikhil13012000@gmail.com>
2020-10-03 12:19:49 +0530

    reconfigure

--- server/build/bundle.js
+++ server/build/bundle.js
@@ -89,8 +89,8 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 var app = (0, _express2.default)();
 app.use(_express2.default.static("public"));
 
-app.get("/", function (req, res) {
-  res.send((0, _renderer2.default)());
+app.get("*", function (req, res) {
+  res.send((0, _renderer2.default)(req));
 });
 
 app.listen(3000, function () {
@@ -120,14 +120,20 @@ var _react2 = _interopRequireDefault(_react);
 
 var _server = __webpack_require__(4);
 
-var _Home = __webpack_require__(5);
+var _reactRouterDom = __webpack_require__(6);
 
-var _Home2 = _interopRequireDefault(_Home);
+var _Routes = __webpack_require__(7);
+
+var _Routes2 = _interopRequireDefault(_Routes);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-exports.default = function () {
-  var content = (0, _server.renderToString)(_react2.default.createElement(_Home2.default, null));
+exports.default = function (req) {
+  var content = (0, _server.renderToString)(_react2.default.createElement(
+    _reactRouterDom.StaticRouter,
+    { location: req.path, context: {} },
+    _react2.default.createElement(_Routes2.default, null)
+  ));
 
   return "\n  <html>\n    <head></head>\n      <body>\n      <div id=\"root\">\n        " + content + "\n        </div>\n      </body>\n      <script src='bundle.js'></script>\n    </html>\n  ";
 };
@@ -175,6 +181,43 @@ var Home = function Home() {
 };
 
 exports.default = Home;
+
+/***/ }),
+/* 6 */
+/***/ (function(module, exports) {
+
+module.exports = require("react-router-dom");
+
+/***/ }),
+/* 7 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _react = __webpack_require__(0);
+
+var _react2 = _interopRequireDefault(_react);
+
+var _reactRouterDom = __webpack_require__(6);
+
+var _Home = __webpack_require__(5);
+
+var _Home2 = _interopRequireDefault(_Home);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+exports.default = function () {
+  return _react2.default.createElement(
+    "div",
+    null,
+    _react2.default.createElement(_reactRouterDom.Route, { exact: true, path: "/", component: _Home2.default })
+  );
+};
 
 /***/ })
 /******/ ]);
